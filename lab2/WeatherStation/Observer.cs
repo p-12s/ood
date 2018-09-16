a8fe4679d8d1d82455acb3b0af2da2bcc7b71ee3
Pavel V <mental.notes@yandex.ru>
2018-09-17 00:50:58 +0300

    Добавил программу 2.1 WeatherStation

---
+++ lab2/WeatherStation/Observer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace WeatherStation
+{
+    /*
+    Are you a display and want to be notified? Implement it
+    */
+    interface IObserver<T>
+    {
+        void Update(T data); // virtual/private not valid
+    }
+
+    /*
+    Signs / unsubscribes to alerts,
+    Sends notifications to observers.
+    */
+    interface IObservable<T>
+    {
+        void RegisterObserver(IObserver<T> observer); // ref ? 
+        void NotifyObservers();
+        void RemoveObserver(IObserver<T> observer);// (IObserver<T> & observer) = 0; 
+    };
+
+    /*
+    Universal classes are universal interfaces.
+    It avoids packaging transformations and unpacking-transformations for value types
+    */
+    class CObservable<T> : IObservable<T>
+    {
+        public void RegisterObserver(IObserver<T> observer)
+	    {
+		    m_observers.Add(observer);
+	    }
+
+        public void NotifyObservers()
+	    {
+		    T data = GetChangedData();
+            // auto clone(m_observers); // Cloning as a solution of problem 2
+            foreach (var observer in m_observers)
+            {
+                observer.Update(data);
+            }
+        }
+
+        public void RemoveObserver(IObserver<T> observer)
+	    {
+		    m_observers.Remove(observer);
+	    }
+
+        protected virtual T GetChangedData()
+        {
+            return default(T);
+        }
+
+        private HashSet<IObserver<T>> m_observers = new HashSet<IObserver<T>>();
+    }
+}
+
